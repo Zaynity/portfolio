06c84165e22efe16f037b66564324690aeb25c5d
Zaynity <zaynity.gamer@gmail.com>
2025-02-12 17:49:53 +0100

    NEW: Add portfolio project

---
+++ src/content/profileData.ts
@@ -0,0 +1,70 @@
+export const PROFILE = {
+  site: {
+    SEO: {
+      //     more complicated description for SEO
+      title: "Matéo MIOSSEC",
+      description:
+        "Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community. | Tech Tim's New Astro Portfolio",
+    },
+  },
+  language: "fr-FR",
+  name: "Matéo MIOSSEC",
+  website: "https://techtim42.com",
+  repo: "https://github.com/tim-hub/techtim-astro-bento-portfolio",
+  avatarUrl: "../../../public/profile_picture.webp",
+  shortIntros: [
+    "💻 Apprenti ingénieur systèmes | Ingénieur DevOps en devenir",
+    "👨‍🎓 Étudiant en dernière année de mastère",
+    "👟 Coureur | 🥊 Combattant MMA | ♟️ Joueur d'échecs",
+  ],
+  words: ["As each one has received a gift, minister it to one another"],
+  technicalSkills: [
+    "Administration système Linux",
+    "Base de données",
+    "Docker",
+    "Git",
+    "Scripting",
+    "Virtualisation",
+  ],
+  otherSkills: [
+    "Design graphique",
+    "Documentation",
+    "Formation",
+    "Gestion de projet",
+  ],
+  links: {
+    github: "https://github.com/tim-hub",
+    medium: "https://medium.com/@TechTim42",
+    twitter: "https://twitter.com/TechTim42",
+    bluesky: "https://bsky.app/profile/techtim.bsky.social",
+    thread: "https://www.threads.net/@techtim2024",
+    mastodon: "https://mastodon.social/@techtim42",
+    youtube: "https://www.youtube.com/@techtim42",
+    stackoverflow: "https://stackoverflow.com/users/5249708/tim",
+    devto: "https://dev.to/timhub",
+    goodreads: "",
+  },
+  studies: [
+    // studies, qualifications and certifications
+    {
+      title: "Master of Applied Computing",
+      level: "Master",
+    },
+    {
+      title: "AWS Cloud Professional",
+      institution: "AWS",
+    },
+    {
+      title: "Neo4j Certified Professional",
+      institution: "Neo4j",
+    },
+    {
+      title: "ICAgile Certified Professional",
+      institution: "ICP",
+    },
+    {
+      title: "Full Stack Observability Practitioner",
+      institution: "New Relic",
+    },
+  ],
+};
