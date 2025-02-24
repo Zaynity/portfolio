b90683afe60cf62445907dcce7aff11d6ebc472a
Zaynity <zaynity.gamer@gmail.com>
2025-02-24 20:25:42 +0100

    UPD: Update experiences content

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,20 +17,8 @@ const projectCollection = defineCollection({
   }),
 });
 
-const experienceCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    startDate: z.date(),
-    endDate: z.date().optional(),
-    description: z.string(),
-    tags: z.array(z.string()).optional(),
-    imageUrl: z.string().optional(),
-  }),
-});
 
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   projects: projectCollection,
-  experiences: experienceCollection,
 };
