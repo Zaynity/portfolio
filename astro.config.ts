1285f29d3fd4c3a2b8814e3bc68eeccb7ee5906e
Zaynity <zaynity.gamer@gmail.com>
2025-03-13 19:31:24 +0100

    UPD: Add base url for astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zaynity.github.io',
+  base: 'portfolio',
   integrations: [
     react(),
     tailwind({
