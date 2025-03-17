e5ea53101fa6a210acc809bc3a9ec84d202d6e74
Zaynity <zaynity.gamer@gmail.com>
2025-03-17 22:33:00 +0100

    FIX: Base is useless with custom domain

--- astro.config.ts
+++ astro.config.ts
@@ -6,8 +6,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mateo-miossec.me',
-  // base: 'portfolio',
-  trailingSlash: "always",
   integrations: [
     react(),
     tailwind({
